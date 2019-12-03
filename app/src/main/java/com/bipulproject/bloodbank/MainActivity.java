4406d2c2b3a757844d933c14abcdaa7b5b0f67b1
bipul <bipulkansakar@gmail.com>
2019-12-03 13:31:21 +0545

    login page added

--- app/src/main/java/com/bipulproject/bloodbank/MainActivity.java
+++ app/src/main/java/com/bipulproject/bloodbank/MainActivity.java
@@ -2,13 +2,53 @@ package com.bipulproject.bloodbank;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText Name;
+    private EditText Password;
+    private TextView Info;
+    private Button Login;
+    private int  counter = 5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Name = (EditText)findViewById(R.id.etEmail);
+        Password = (EditText)findViewById(R.id.etPassword);
+        Info = (TextView) findViewById(R.id.tvInfo);
+        Login = (Button)findViewById(R.id.btnLogin);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validate(Name.getText().toString(),Password.getText().toString());
+            }
+        });
+
+    }
+
+    private void validate(String userName, String userPassword){
+        if((userName =="Admin") && (userPassword == ("1234"))){
+
+
+        Intent intent = new Intent(MainActivity.this, SecondActivity.class);
+        startActivity(intent);
+    }else{
+            counter--;
+
+            if (counter == 0){
+                Login.setEnabled(false);
+            }
+
+        }
     }
 }
